e67d50f0b3a5564e9755587d0c855688457dc4dc
YizheChenUT <yz.chen@mail.utoronto.ca>
2024-09-24 16:21:15 +0000

    Updaate: 1 Simulates reported crime data, such as number of incidents, type of crime and regional distribution.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,33 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates reported crime data, such as number of incidents, type of crime and regional distribution.
+# Author: Yizhe Chen
+# Date: 24 SEP 2024
+# Contact: yz.chen@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: N/A
+# Any other information needed? No
 
 
 #### Workspace setup ####
-library(tidyverse)...
-# [...UPDATE THIS...]
+library(tidyverse)
+
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(304) #Setting seed to ensure reproducibility
 
+## Simulate crime data sets
+simulate_crime_data <- function(n = 1000) {
+  data.frame(
+    crime_id = 1:n,
+    crime_type = sample(c("Assault", "Robbery", "Theft", "Fraud"), n, replace = TRUE),
+    region = sample(c("Downtown", "Midtown", "North", "East", "West"), n, replace = TRUE),
+    incidents = rpois(n, lambda = 5)
+  )
+}
+
+simulated_crime_data <- simulate_crime_data()
+
+##Save crime data
+write.csv(simulated_crime_data, "data/raw_data/simulated_reported_crimes.csv", row.names = FALSE)
 
 
