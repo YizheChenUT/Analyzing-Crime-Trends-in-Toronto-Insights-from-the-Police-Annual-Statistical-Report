7f278e5790fef7214432a4f8ffdb900d718ff085
YizheChenUT <yz.chen@mail.utoronto.ca>
2024-09-25 03:28:45 +0000

    test

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,11 +1,11 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests simulated dataset.
+# Author: Yizhe Chen
+# Date: 24 SEP 2024
+# Contact: yz.chen@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Run 00-simulate_data.R 
+# Any other information needed? No
 
 
 #### Workspace setup ####
